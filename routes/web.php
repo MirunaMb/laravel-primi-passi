5edee7891ae6e04b61304b3cdb962b5e3e8f25b7
MirunaMb <mirunamarcela98@gmail.com>
2023-10-11 20:29:29 +0200

    add BONUS

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('homepage');
-});
+})->name('homepage');
+Route::get('contact', function () {
+    return view('contact');
+})->name('contact');
+Route::get('about', function () {
+    return view('about');
+})->name('about');
